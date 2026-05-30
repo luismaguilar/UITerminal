8928c1518512aefd58c68015fb5bb1c6b18e7ee3
luismaguilar <84119255+luismaguilar@users.noreply.github.com>
2026-05-30 16:26:51 -0500

    Initial menu layout

--- uiterminal/src/App.jsx
+++ uiterminal/src/App.jsx
@@ -1,9 +1,11 @@
+import Menu from "./components/Menu"
+
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div className="bg-amber-800 h-screen w-screen flex flex-row min-h-screen justify-center items-center">
+      <Menu></Menu>
+    </div>
   )
 }
 
--- uiterminal/src/components/Menu.jsx
+++ uiterminal/src/components/Menu.jsx
@@ -1,8 +1,20 @@
 function Menu() {
     return (
-        <>
-        
-        </>
+        <div className="bg-neutral-950 font-mono w-fit">
+            <img src=".\src\assets\luisLogo.png" className="object-none p-40"></img>
+            <br/>
+           <div className="p-10 text-gray-700">
+                <div>
+                    <button className="text-green-400 text-left">about</button><p className="float-right">About Me</p>
+                </div>
+                <div>
+                    <button className="text-green-400">projects</button><p className="float-right">Check out! <span>projects</span></p>
+                </div>
+                <div>
+                    <button className="text-green-400">contact</button><p className="float-right">Email me!</p>
+                </div>
+                </div>
+            </div>
     )
 }
 
