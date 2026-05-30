03654df02340ee27cbbdd33828d175d450b4e5f8
luismaguilar <84119255+luismaguilar@users.noreply.github.com>
2026-05-30 16:46:50 -0500

    Color changes and layout edits

--- uiterminal/src/App.jsx
+++ uiterminal/src/App.jsx
@@ -3,7 +3,7 @@ import Menu from "./components/Menu"
 function App() {
 
   return (
-    <div className="bg-amber-800 h-screen w-screen flex flex-row min-h-screen justify-center items-center">
+    <div className="bg-neutral-950 h-screen w-screen flex flex-row min-h-screen justify-center items-center">
       <Menu></Menu>
     </div>
   )
