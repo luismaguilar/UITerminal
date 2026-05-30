388ed79f76f385a1144b72dba9b4e1abe8164fb2
luismaguilar <84119255+luismaguilar@users.noreply.github.com>
2026-05-30 17:22:27 -0500

    Basic terminal window added

--- uiterminal/src/App.jsx
+++ uiterminal/src/App.jsx
@@ -1,10 +1,15 @@
 import Menu from "./components/Menu"
+import Terminal from "./components/Terminal"
 
 function App() {
 
   return (
-    <div className="bg-neutral-950 h-screen w-screen flex flex-row min-h-screen justify-center items-center">
-      <Menu></Menu>
+    <div className="bg-neutral-950 h-screen w-screen flex flex-col min-h-screen justify-center items-center">
+      <div>
+        <Menu></Menu>
+        <br/>
+        <Terminal></Terminal>
+      </div>
     </div>
   )
 }
---
+++ uiterminal/src/components/Terminal.jsx
@@ -0,0 +1,12 @@
+function Terminal() {
+    return (
+        <div className="bg-neutral-950 font-mono border-3 border-gray-500" >
+        <p className="text-gray-700 p-1 text-1">Type the above options above in the terminal or click the options instead.</p>
+            <div className="bg-neutral-950 font-mono border-t-3 border-gray-500" >
+                <p className="text-green-300 text-left p-1">luis@porfolio:~$ Type command here</p>
+            </div>
+        </div>
+    )
+}
+
+export default Terminal
